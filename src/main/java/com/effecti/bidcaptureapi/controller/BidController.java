dbdea33fd3fae02624dda1ae1babcd9bc3d64e36
coelhocj <cristiano.coelho@acantolabs.com>
2022-03-05 11:27:10 -0300

    Add cross anotation

--- src/main/java/com/effecti/bidcaptureapi/controller/BidController.java
+++ src/main/java/com/effecti/bidcaptureapi/controller/BidController.java
@@ -8,6 +8,7 @@ import com.effecti.bidcaptureapi.service.BidService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api-bids/bids")
 public class BidController {
 
